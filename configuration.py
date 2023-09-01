7c576d4ad58d1bcc27c37af2554a85a2592239c2
Sveta <ms.skuznetsova@mail.ru>
2023-09-01 16:28:50 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://9d9f3202-d713-4d3f-9c83-d5923a89ae11.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
